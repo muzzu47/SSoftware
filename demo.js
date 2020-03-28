a39666e79cb21e4f063be588c91d8ee1481f640b
muzzu47 <47438293+muzzu47@users.noreply.github.com>
2020-03-28 10:51:32 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,34 @@
+const muzzu = () => {
+    let a = "string a ";
+   const b =() => {
+        let c = "string c";
+        let d = a + c;
+        // console.log(d);
+        return d;
+    }
+
+    // return {
+    //     b : b
+    // };
+    return b;
+}
+
+// muzzu(); 
+// b();
+// muzzu();
+// muzzu();
+// muzzu()();
+
+let z = muzzu()();
+// console.log(z);
+let f = () => {
+    let g = "string g";
+    let k = z + ' ' + g;
+    console.log(k);
+}
+
+f();
+
+// const sana = muzzu();
+
+// sana.b();
